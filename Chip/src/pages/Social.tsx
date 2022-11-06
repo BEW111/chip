d61768ccd0d5d56591f1e857f6ac4e04fcf69f1d
Brian Williams <bewilliams111+github@gmail.com>
2022-11-06 15:08:51 -0500

    reminders started

--- Chip/src/pages/Social.tsx
+++ Chip/src/pages/Social.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import {View, ScrollView, Text} from 'react-native';
-import {IconButton} from 'react-native-paper';
+import {Button, IconButton, Modal, Portal} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
+import { scheduleNotification, onDisplayNotification, onCreateTriggerNotification, requestNotificationsPermission } from '../utils/notifcations';
+
 export default function Social() {
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20, width: '90%', alignSelf: 'center'};
+
+
+
   return (
     <SafeAreaView>
       <View style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Social view area</Text>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+            <Text>Example Modal.  Click outside this area to dismiss.</Text>
+
+            <Button icon="notifications" mode="contained" onPress={() => {
+              var soon = new Date();
+              soon.setSeconds(soon.getSeconds() + 10);
+              console.log(soon.toDateString());
+
+              scheduleNotification(soon, 'Prep for workout', 'Set your clothes out for the gym tomorrow');
+            }}>
+              Schedule notifcation
+            </Button>
+          </Modal>
+        </Portal>
+        <Button mode="contained" style={{marginTop: 30}} onPress={showModal}>
+          Set reminder
+        </Button>
       </View>
     </SafeAreaView>
   )
--- Chip/src/utils/notifcations.ts
+++ Chip/src/utils/notifcations.ts
@@ -1,8 +1,20 @@
-import notifee, { TimestampTrigger, TriggerType } from '@notifee/react-native';
+import notifee, { TimestampTrigger, TriggerType, AuthorizationStatus } from '@notifee/react-native';
 
+
+export async function requestNotificationsPermission() {
+  const settings = await notifee.requestPermission();
+
+  if (settings.authorizationStatus >= AuthorizationStatus.AUTHORIZED) {
+    console.log('Permission settings:', settings);
+  } else {
+    console.log('User declined permissions');
+  }
+}
 
 // Schedule a generic notifcation for a specific time
 export async function scheduleNotification(date: Date, title: string, body: string) {
+  console.log('scheduling notification');
+
   // Create a time-based trigger
   const trigger: TimestampTrigger = {
     type: TriggerType.TIMESTAMP,
@@ -20,4 +32,59 @@ export async function scheduleNotification(date: Date, title: string, body: stri
     },
     trigger,
   );
+
+  console.log('notification scheduled');
+}
+
+export async function onCreateTriggerNotification(date) {
+  const date1 = new Date();
+  date1.setHours(10);
+  date1.setMinutes(32);
+
+  console.log(date1);
+
+  // Create a time-based trigger
+  const trigger: TimestampTrigger = {
+    type: TriggerType.TIMESTAMP,
+    timestamp: date.getTime(),
+  };
+
+  // Create a trigger notification
+  const scheduleResult = await notifee.createTriggerNotification(
+    {
+      title: 'Scheduled notification',
+      body: 'Today at ______',
+      android: {
+        channelId: 'your-channel-id',
+      },
+    },
+    trigger,
+  );
+
+  console.log(scheduleResult);
+}
+
+export async function onDisplayNotification() {
+  // Request permissions (required for iOS)
+  await notifee.requestPermission()
+
+  // Create a channel (required for Android)
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+  });
+
+  // Display a notification
+  await notifee.displayNotification({
+    title: 'Notification Title',
+    body: 'Main body content of the notification',
+    android: {
+      channelId,
+      smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+      // pressAction is needed if you want the notification to open the app when pressed
+      pressAction: {
+        id: 'default',
+      },
+    },
+  });
 }
